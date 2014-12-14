779bb66f493218ee8c26d07f0fa0e96a5dd9e8ce
Burak Alparslan <burak.alparslan@monitise.com>
2014-12-14 17:49:57 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
-var oxiNewsApp = angular.module('oxiNewsApp', []);
+var endpoint = "http://localhost:8080/OXIWebService";
+var oxiNewsApp = angular.module('oxiNewsApp', ["ui.bootstrap"]);
+
+
+oxiNewsApp.controller("navMainController", function($scope, $http) {
+    $http.get(endpoint + "/getMenu")
+        .success(function(response) {$scope.menu = response;});
+});
 
 oxiNewsApp.controller('NewListController', function ($scope, $http) {
-	$http.get("http://localhost:8080/OXIWebService/getNews")
-  .success(function(response) {$scope.news = response;});
+	$http.get(endpoint + "/getNews")
+        .success(function(response) {$scope.news = response;});
+
+});
+
+oxiNewsApp.controller('CarouselCtrl', function ($scope, $http) {
+
+    $scope.myInterval = 5000;
+	$http.get(endpoint + "/getNews")
+        .success(function(response) {
+
+            $scope.news = response;
+
+        });
 
 });
 
 oxiNewsApp.controller("categoryController", function($scope, $http) {
-  $http.get("http://localhost:8080/OXIWebService/getCategories")
-  .success(function(response) {$scope.categories = response;});
+    $http.get(endpoint +  "/getCategories")
+        .success(function(response) {$scope.categories = response;});
 });
